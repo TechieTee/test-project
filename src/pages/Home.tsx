b653f409abf6045c5aafc3bb228041e4e9d9edec
Fatima Modupe Bello <mailtomodupe@gmail.com>
2025-01-19 00:33:29 +0100

    updates homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,24 @@
 import { useState } from 'react'
-import {Image} from '../assets'
+import { Image } from '../assets'
 import viteLogo from '/vite.svg'
 import '../App.css'
 
 
 const Home = () => {
-    const [count, setCount] = useState(0)
-    return( 
+  const [count, setCount] = useState(0)
+  return (
     <>
-  
-      <h1>Welcome to the Home Page!</h1>
-     <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={Image.reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
-      <code>Vite + React write it well</code>
 
+      <h1>Welcome to the Home Page!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
     </>
-  
 
-)
-  };
-  
-  export default Home;
-  
+
+  )
+};
+
+export default Home;
